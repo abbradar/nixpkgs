8dd76c6b3bd5f4c9862a0996cfd3a04b60ffbe7d
Nikolay Amiantov <ab@fmap.me>
2015-06-12 01:59:11 +0300

    udev-145: remove old package

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -10007,7 +10007,6 @@ let
     cross = assert crossSystem != null; crossSystem;
   });
 
-  udev145 = callPackage ../os-specific/linux/udev/145.nix { };
   udev = pkgs.systemd;
   eudev = callPackage ../os-specific/linux/eudev {};
 
