38700e43b260670ac93f87f256f00cf38c50e83a
Nikolay Amiantov <ab@fmap.me>
2015-06-25 13:00:30 +0300

    Revert "steam: use native libraries, disable steam-runtime"
    This reverts commit 2c8ec5fb436243657c6138ab4761036ad15bfc41.

--- pkgs/games/steam/chrootenv.nix
+++ pkgs/games/steam/chrootenv.nix
@@ -5,86 +5,72 @@ buildFHSUserEnv {
 
   targetPkgs = pkgs:
     [ pkgs.steam-original
-      # Errors in output without those
-      pkgs.pciutils
-      pkgs.python2
+      pkgs.corefonts
+      pkgs.curl
+      pkgs.dbus
+      pkgs.dpkg
+      pkgs.mono
+      pkgs.python
+      pkgs.gnome2.zenity
+      pkgs.xdg_utils
     ]
     ++ lib.optional (config.steam.java or false) pkgs.jdk
     ++ lib.optional (config.steam.primus or false) pkgs.primus
     ;
 
   multiPkgs = pkgs:
-    [ # These are required by steam with proper errors
-      pkgs.xlibs.libXcomposite
-      pkgs.xlibs.libXtst
-      pkgs.xlibs.libXrandr
-      pkgs.xlibs.libXext
-      pkgs.xlibs.libX11
-      pkgs.xlibs.libXfixes
-
+    [ pkgs.cairo
       pkgs.glib
-      pkgs.gtk2
-      pkgs.bzip2
-      pkgs.zlib
-      pkgs.libpulseaudio
+      pkgs.gtk
       pkgs.gdk_pixbuf
-
-      # Without these it silently fails
-      pkgs.xlibs.libXinerama
-      pkgs.xlibs.libXdamage
-      pkgs.xlibs.libXcursor
-      pkgs.xlibs.libXrender
-      pkgs.xlibs.libXi
-      pkgs.xlibs.libSM
-      pkgs.xlibs.libICE
-      pkgs.gnome2.GConf
-      pkgs.freetype
-      pkgs.openalSoft
-      pkgs.curl
-      pkgs.nspr
-      pkgs.nss
-      pkgs.fontconfig
-      pkgs.cairo
       pkgs.pango
-      pkgs.alsaLib
-      pkgs.expat
-      pkgs.dbus
-      pkgs.cups
-      pkgs.libcap
-      pkgs.SDL2
-      pkgs.libusb1
-      pkgs.dbus_glib
-      pkgs.ffmpeg
-      # Only libraries are needed from those two
-      pkgs.udev182
-      pkgs.networkmanager098
 
-      # Games requirements
-      pkgs.xlibs.libXmu
+      pkgs.freetype
+      pkgs.xlibs.libICE
+      pkgs.xlibs.libSM
+      pkgs.xlibs.libX11
+      pkgs.xlibs.libXau
       pkgs.xlibs.libxcb
-      pkgs.xlibs.libpciaccess
-      pkgs.gst_all_1.gst-plugins-ugly
-      pkgs.mesa_glu
-      pkgs.libuuid
-      pkgs.libogg
-      pkgs.libvorbis
+      pkgs.xlibs.libXcursor
+      pkgs.xlibs.libXdamage
+      pkgs.xlibs.libXdmcp
+      pkgs.xlibs.libXext
+      pkgs.xlibs.libXfixes
+      pkgs.xlibs.libXi
+      pkgs.xlibs.libXinerama
+      pkgs.xlibs.libXrandr
+      pkgs.xlibs.libXrender
+      pkgs.xlibs.libXScrnSaver
+      pkgs.xlibs.libXtst
+      pkgs.xlibs.libXxf86vm
+
+      pkgs.ffmpeg
+      pkgs.libpng12
+      pkgs.mesa
       pkgs.SDL
-      pkgs.SDL2_image
-      pkgs.glew110
-      pkgs.openssl
-      pkgs.libidn
+      pkgs.SDL2
+
+      pkgs.libgcrypt
+      pkgs.zlib
+
+      pkgs.alsaLib
+      pkgs.libvorbis
+      pkgs.openal
+      pkgs.libpulseaudio
+
+      pkgs.flashplayer
+
+      pkgs.gst_all_1.gst-plugins-ugly # "Audiosurf 2" needs this
     ];
 
   extraBuildCommandsMulti = ''
     cd usr/lib
     ln -sf ../lib64/steam steam
-
-    # FIXME: maybe we should replace this with proper libcurl-gnutls
-    ln -s libcurl.so.4 libcurl-gnutls.so.4
   '';
 
   profile = ''
-    export STEAM_RUNTIME=0
+    # Ugly workaround for https://github.com/ValveSoftware/steam-for-linux/issues/3504
+    export LD_PRELOAD=/lib32/libpulse.so:/lib64/libpulse.so:/lib32/libasound.so:/lib64/libasound.so
   '';
 
   runScript = "steam";
