ed76e91bdb4216087d4895af5c052d92fc2779c7
Nikolay Amiantov <ab@fmap.me>
2015-06-29 14:18:20 +0300

    qtox: update, rework dependencies

--- pkgs/applications/networking/instant-messengers/qtox/default.nix
+++ pkgs/applications/networking/instant-messengers/qtox/default.nix
@@ -1,24 +1,39 @@
-{ stdenv, fetchgit, pkgconfig, libtoxcore-dev, qt5, openal, opencv,
-  libsodium, libXScrnSaver, glib, gdk_pixbuf, gtk2, cairo,
-  pango, atk, qrencode, ffmpeg, filter-audio }:
+{ stdenv, fetchFromGitHub, pkgconfig, libtoxcore, qt5, openalSoft
+, libsodium, libXScrnSaver, glib, gdk_pixbuf, gtk2, cairo, pango, atk
+, qrencode, ffmpeg, filter_audio }:
 
 stdenv.mkDerivation rec {
-  name = "qtox-dev-20150624";
+  name = "qtox-${version}";
+  version = "1.1";
 
-  src = fetchgit {
-      url = "https://github.com/tux3/qTox.git";
-      rev = "9f386135a2cf428d2802b158c70be4beee5abf86";
-      sha256 = "1m2y50q5yim1q75k48cy5daq5qm77cvb3kcla7lpqv54xnfdwxk8";
+  src = fetchFromGitHub {
+    owner = "tux3";
+    repo = "qTox";
+    rev = "v${version}";
+    sha256 = "1i9q3s6fm6w7njzrvmhy2dbdj0x4skpx1c7m6r1hjlhmii8ywbsn";
   };
+
+  NIX_CFLAGS_COMPILE = [ "-I${glib}/lib/glib-2.0/include"
+                         "-I${glib}/include/glib-2.0"
+                         "-I${gdk_pixbuf}/include/gdk-pixbuf-2.0"
+                         "-I${gtk2}/include/gtk-2.0"
+                         "-I${gtk2}/lib/gtk-2.0/include"
+                         "-I${cairo}/include/cairo"
+                         "-I${pango}/include/pango-1.0"
+                         "-I${atk}/include/atk-1.0"
+                       ];
 
   buildInputs =
     [
-      libtoxcore-dev openal opencv libsodium filter-audio
-      qt5.base qt5.tools libXScrnSaver glib gtk2 cairo
-      pango atk qrencode ffmpeg qt5.translations
+      libtoxcore openalSoft filter_audio qt5.base
+      glib libXScrnSaver gdk_pixbuf gtk2 cairo
+      libsodium qrencode ffmpeg
     ];
+  nativeBuildInputs = [ qt5.tools ];
 
-  nativeBuildInputs = [ pkgconfig ];
+  postPatch = ''
+    #sed -i "s,/usr/,/this-does-not-exist/,g" qtox.pro
+  '';
 
   preConfigure = ''
     export NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE $(pkg-config --cflags glib-2.0)"
