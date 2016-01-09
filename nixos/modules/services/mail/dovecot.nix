aefec8d1e03161c9e8a34ff1a902c37a3d130a06
Nikolay Amiantov <ab@fmap.me>
2016-01-09 04:15:25 +0300

    nixos/dovecot: use systemd's RuntimeDirectory instead of creating it by ourselves

--- nixos/modules/services/mail/dovecot.nix
+++ nixos/modules/services/mail/dovecot.nix
@@ -202,8 +202,6 @@ in
       wantedBy = [ "multi-user.target" ];
 
       preStart = ''
-        mkdir -p "${baseDir}/login"
-        chown -R ${cfg.user}:${cfg.group} "${baseDir}"
         rm -f "${stateDir}/modules"
         ln -s "${modulesDir}" "${stateDir}/modules"
       '';
@@ -214,6 +212,7 @@ in
         Restart = "on-failure";
         RestartSec = "1s";
         StartLimitInterval = "1min";
+        RuntimeDirectory = [ "dovecot2" ];
       };
     };
 
