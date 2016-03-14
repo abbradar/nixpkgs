48595bb1526cda44cc51e96daa714f332672625f
Nikolay Amiantov <ab@fmap.me>
2016-03-15 00:56:23 +0300

    eduke32: 20150420-5160 -> 20160313-5669

--- pkgs/games/eduke32/default.nix
+++ pkgs/games/eduke32/default.nix
@@ -1,21 +1,26 @@
-{ stdenv, fetchurl, flac, gtk, libvorbis, libvpx, makeDesktopItem, mesa, nasm
+{ stdenv, lib, fetchurl, flac, gtk, libvorbis, libvpx, makeDesktopItem, mesa, nasm
 , pkgconfig, SDL2, SDL2_mixer }:
 
 let
-  year = "2015";
-  date = "20150420";
-  rev = "5160";
+  date = "20160313";
+  rev = "5669";
+
+  year = lib.substring 0 4 date;
+
 in stdenv.mkDerivation rec {
   name = "eduke32-${version}";
   version = "${date}-${rev}";
 
   src = fetchurl {
-    url = "http://dukeworld.duke4.net/eduke32/synthesis/old/${year}/${version}/eduke32_src_${version}.tar.xz";
-    sha256 = "1nlq5jbglg00c1z1vsyl627fh0mqfxvk5qyxav5vzla2b4svik2v";
+    urls = [
+      "http://dukeworld.duke4.net/eduke32/synthesis/${version}/eduke32_src_${version}.tar.xz"
+      "http://dukeworld.duke4.net/eduke32/synthesis/old/${year}/${version}/eduke32_src_${version}.tar.xz"
+    ];
+    sha256 = "0xhk5cr1ww5ar0mk9pfvyx0c89qv8xj1zw41plq758mz9vw3ajrm";
   };
 
   buildInputs = [ flac gtk libvorbis libvpx mesa SDL2 SDL2_mixer ]
-    ++ stdenv.lib.optional (stdenv.system == "i686-linux") nasm;
+    ++ lib.optional (stdenv.system == "i686-linux") nasm;
   nativeBuildInputs = [ pkgconfig ];
 
   postPatch = ''
@@ -24,8 +29,8 @@ in stdenv.mkDerivation rec {
       --replace libGLU.so	${mesa}/lib/libGLU.so
   '';
 
-  NIX_CFLAGS_COMPILE = "-I${SDL2}/include/SDL";
-  NIX_LDFLAGS = "-L${SDL2}/lib";
+  NIX_CFLAGS_COMPILE = [ "-I${SDL2}/include/SDL" ];
+  NIX_LDFLAGS = [ "-L${SDL2}/lib" ];
 
   makeFlags = [
     "LINKED_GTK=1"
@@ -47,10 +52,10 @@ in stdenv.mkDerivation rec {
     cat > eduke32-wrapper <<EOF
     #!/bin/sh
 
-    if [ "$EDUKE32_DATA_DIR" = "" ]; then
+    if [ "\$EDUKE32_DATA_DIR" = "" ]; then
         EDUKE32_DATA_DIR=/var/lib/games/eduke32
     fi
-    if [ "$EDUKE32_GRP_FILE" = "" ]; then
+    if [ "\$EDUKE32_GRP_FILE" = "" ]; then
         EDUKE32_GRP_FILE=\$EDUKE32_DATA_DIR/DUKE3D.GRP
     fi
 
@@ -71,5 +76,6 @@ in stdenv.mkDerivation rec {
     license = licenses.gpl2Plus;
     homepage = http://eduke32.com;
     maintainers = with maintainers; [ nckx sander ];
+    platforms = platforms.linux;
   };
 }
