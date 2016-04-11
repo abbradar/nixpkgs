61e79a012f988f09059abafe54933cc9e717706b
Nikolay Amiantov <ab@fmap.me>
2016-04-11 18:20:02 +0300

    openmw: don't override mygui

--- pkgs/games/openmw/default.nix
+++ pkgs/games/openmw/default.nix
@@ -4,8 +4,6 @@ stdenv.mkDerivation rec {
   version = "0.38.0";
   name = "openmw-${version}";
 
-  mygui_ = mygui.override { withOgre = false; } ;
-
   src = fetchFromGitHub {
     owner = "OpenMW";
     repo = "openmw";
@@ -13,11 +11,12 @@ stdenv.mkDerivation rec {
     sha256 = "1ssz1pa59a34v5vxiccqyvij5s38kl662p7xbc59y90y668f78y6";
   };
 
-  buildInputs = [ cmake boost ffmpeg qt4 bullet mygui_ openscenegraph SDL2 unshield openal pkgconfig ];
+  buildInputs = [ cmake boost ffmpeg qt4 bullet mygui openscenegraph SDL2 unshield openal pkgconfig ];
 
-  meta = {
+  meta = with stdenv.lib; {
     description = "An unofficial open source engine reimplementation of the game Morrowind";
     homepage = "http://openmw.org";
-    license = stdenv.lib.licenses.gpl3;
+    platforms = platforms.linux;
+    license = licenses.gpl3;
   };
 }
