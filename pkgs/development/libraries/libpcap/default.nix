d2bc78438ebac29c2008e9b3f6e1eef44da81c46
Nikolay Amiantov <ab@fmap.me>
2023-05-22 18:44:45 +0700

    Revert "libpcap: propagate libnl"
    This reverts commit 66445a4e9950bdb369c174b84d36258214cb9b0b.
    
    Apparently it is a huge mass rebuild.

--- pkgs/development/libraries/libpcap/default.nix
+++ pkgs/development/libraries/libpcap/default.nix
@@ -20,10 +20,8 @@ stdenv.mkDerivation rec {
     hash = "sha256-7RmgOD+tcuOtQ1/SOdfNgNZJFrhyaVUBWdIORxYOvl8=";
   };
 
-  buildInputs = lib.optionals withRemote [ libxcrypt ];
-
-  # pkg-config dependency.
-  propagatedBuildInputs = lib.optionals stdenv.isLinux [ libnl ];
+  buildInputs = lib.optionals stdenv.isLinux [ libnl ]
+    ++ lib.optionals withRemote [ libxcrypt ];
 
   nativeBuildInputs = [ flex bison ]
     ++ lib.optionals stdenv.isLinux [ pkg-config ]
