9b4a60d9791bebf002ec5c22ad2de37b40639090
Nikolay Amiantov <ab@fmap.me>
2025-03-30 19:41:06 +0700

    ntfs module: support ntfs in initrd via ntfs3

--- nixos/modules/tasks/filesystems/ntfs.nix
+++ nixos/modules/tasks/filesystems/ntfs.nix
@@ -7,12 +7,19 @@
 
 with lib;
 
+let
+  inInitrd = config.boot.initrd.supportedFilesystems.ntfs or config.boot.initrd.supportedFilesystems.ntfs-3g or false;
+in
 {
   config =
     mkIf (config.boot.supportedFilesystems.ntfs or config.boot.supportedFilesystems.ntfs-3g or false)
       {
 
         system.fsPackages = [ pkgs.ntfs3g ];
+
+        boot.initrd.availableKernelModules = mkIf inInitrd [
+          "ntfs3"
+        ];
 
       };
 }
